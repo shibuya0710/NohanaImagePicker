babab422bd1ab06df0cfd9bc08513437139b30f8
shibuya0710 <146042049+shibuya0710@users.noreply.github.com>
2023-09-26 09:37:34 +0900

    Update AssetDateSectionHeaderView.swift

--- NohanaImagePicker/AssetDateSectionHeaderView.swift
+++ NohanaImagePicker/AssetDateSectionHeaderView.swift
@@ -71,8 +71,8 @@ class AssetDateSectionHeaderView: UICollectionReusableView {
         self.assets = assets
         self.nohanaImagePickerController = nohanaImagePickerController
         if pickButton.image(for: UIControl.State()) == nil, pickButton.image(for: .selected) == nil {
-            let droppedImage: UIImage? = nohanaImagePickerController.config.image.droppedLarge ?? UIImage(named: "btn_select_l", in: nohanaImagePickerController.assetBundle, compatibleWith: nil)
-            let pickedImage: UIImage? = nohanaImagePickerController.config.image.pickedLarge ?? UIImage(named: "btn_selected_l", in: nohanaImagePickerController.assetBundle, compatibleWith: nil)
+            let droppedImage: UIImage? = UIImage(named: "btn_select_l")
+            let pickedImage: UIImage? = UIImage(named: "btn_selected_l")
             pickButton.setImage(droppedImage, for: UIControl.State())
             pickButton.setImage(pickedImage, for: .selected)
         }
